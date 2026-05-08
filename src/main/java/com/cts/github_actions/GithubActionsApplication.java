b27f0213dd298b3cce946d3389b394594aa53f78
Adhav <2463677@cognizant.com>
2026-05-08 20:12:48 +0530

    code

---
+++ src/main/java/com/cts/github_actions/GithubActionsApplication.java
@@ -0,0 +1,22 @@
+package com.cts.github_actions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GithubActionsApplication {
+
+    @GetMapping("/hello")
+    public String welcome(){
+        return "Hello world !";
+    }
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubActionsApplication.class, args);
+	}
+
+}
